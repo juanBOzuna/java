7fd90f8fa21faada97f6bc787e9c9f46ef237ac2
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2021-08-31 17:59:11 -0500

    feat: finish learning 0.1

---
+++ ejercicios_java/step_4/Step.java
@@ -0,0 +1,11 @@
+package step_4;
+
+import javax.swing.JOptionPane;
+
+public class Step {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+    
+}
