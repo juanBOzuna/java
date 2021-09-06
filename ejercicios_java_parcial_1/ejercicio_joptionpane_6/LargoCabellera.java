826f61a432b49b06b8e15dcbdcf7e99897c466a9
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2021-09-06 18:09:09 -0500

    finish ejercicio-joptionpane-6-parcial1

---
+++ ejercicios_java_parcial_1/ejercicio_joptionpane_6/LargoCabellera.java
@@ -0,0 +1,26 @@
+package ejercicio_joptionpane_6;
+
+import javax.swing.JOptionPane;
+
+public class LargoCabellera {
+    public static void main(String[] args) {
+        double cm = 0, metros = 0;
+        int edad = 0;
+        boolean validador = true;
+
+        while (validador) {
+            try {
+                edad = Integer.parseInt(JOptionPane.showInputDialog("Digite su edad"));
+                validador = false;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Debe ser solo numeros");
+            }
+        }
+
+        metros = ((edad * 360) / 30) * 4;
+
+        JOptionPane.showMessageDialog(null, "su pelo asumiendo que nunca se lo ha cortado deberia medir "
+                + String.format("%.02f", metros).replace(".", ",") + " metros");
+
+    }
+}
