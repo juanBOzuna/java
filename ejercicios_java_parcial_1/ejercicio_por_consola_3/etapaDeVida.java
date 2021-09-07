b0c6e7ff0955d2a722fbcba9428cf0c3b0b4c961
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2021-09-06 20:37:57 -0500

    finish bugfix

--- ejercicios_java_parcial_1/ejercicio_por_consola_3/etapaDeVida.java
+++ ejercicios_java_parcial_1/ejercicio_por_consola_3/etapaDeVida.java
@@ -8,74 +8,41 @@ public class etapaDeVida {
         System.out.print("\nDigite su edad ");
         Scanner scan = new Scanner(System.in);
         int edad = scan.nextInt();
+        String etapa = "";
 
-        switch (edad) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                System.out.print("\nUsted esta en primera Infancia");
-                break;
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-                System.out.print("\nUsted esta en la nfancia");
-                break;
-            case 12:
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 17:
-            case 18:
-                System.out.print("\nUsted esta en la adolescencia");
-                break;
-            case 19:
-            case 20:
-            case 21:
-            case 22:
-            case 23:
-            case 24:
-            case 25:
-            case 26:
-                System.out.print("\nUsted esta en la juventud");
-                break;
-            case 27:
-            case 28:
-            case 29:
-            case 30:
-            case 31:
-            case 32:
-            case 33:
-            case 34:
-            case 35:
-                System.out.print("\nusted es un adulto");
-                break;
-            case 36:
-            case 37:
-            case 38:
-            case 39:
-            case 40:
-            case 41:
-            case 42:
-            case 43:
-            case 44:
-            case 45:
-            case 46:
-            case 47:
-            case 48:
-            case 49:
-            case 50:
-                System.out.print("\n usted es un adulto maduro");
-                break;
-            default:
-                System.out.print("\nusted es un adulto mayor");
+        if (edad <= 5) {
+            etapa = "Primer Infante";
+        }
+        if (edad >= 6 && edad <= 11) {
+            etapa = "Infante";
 
         }
+        if (edad >= 12 && edad < 14) {
+            etapa = "Adolescente";
+
+        }
+        if (edad >= 14 && edad < 18) {
+            etapa = "Adolescente Joven";
+
+        }
+        if (edad >= 18 && edad <= 26) {
+            etapa = "Adulto Joven";
+
+        }
+        if (edad > 26 && edad < 59) {
+            etapa = "Adulto";
+
+        }
+        if (edad >= 27 && edad <= 59) {
+            etapa = "Adulto Maduro";
+
+        }
+        if (edad >= 60) {
+            etapa = "Adulto Mayor";
+
+        }
+
+        System.out.print("\nUsted es un" + etapa);
+
     }
 }
